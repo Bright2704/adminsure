854b6082477550332b89697a397c72ad3b3c58e3
Bright2704 <thanonchaikantha27@gmail.com>
2024-06-19 02:06:44 +0700

    http

--- src/page/ResetPassword.jsx
+++ src/page/ResetPassword.jsx
@@ -17,7 +17,7 @@ function ResetPassword() {
         }
 
         try {
-            const response = await fetch('https://210.246.202.185:3000/update-password', {
+            const response = await fetch('http://210.246.202.185:3000/update-password', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
